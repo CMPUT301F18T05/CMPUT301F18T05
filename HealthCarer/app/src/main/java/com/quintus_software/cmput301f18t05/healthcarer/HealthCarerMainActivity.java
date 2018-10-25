ad528cea0d2e2bbef0e6d5508ebc78e305048153
Gregory Neagu <neagu@cs.ualberta.ca>
2018-10-25 14:53:30 -0600

    Added a blaank Android Project to the repository

---
+++ HealthCarer/app/src/main/java/com/quintus_software/cmput301f18t05/healthcarer/HealthCarerMainActivity.java
@@ -0,0 +1,13 @@
+package com.quintus_software.cmput301f18t05.healthcarer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HealthCarerMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_carer_main);
+    }
+}
