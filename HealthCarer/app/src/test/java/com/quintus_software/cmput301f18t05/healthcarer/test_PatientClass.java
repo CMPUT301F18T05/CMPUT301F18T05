adaeaded40ceff8e44bf11b851166e0206864658
gneagu <gneagu@users.noreply.github.com>
2018-11-01 20:19:49 -0600

    Update HealthCarer/app/src/test/java/com/quintus_software/cmput301f18t05/healthcarer/test_PatientClass.java

--- HealthCarer/app/src/test/java/com/quintus_software/cmput301f18t05/healthcarer/test_PatientClass.java
+++ HealthCarer/app/src/test/java/com/quintus_software/cmput301f18t05/healthcarer/test_PatientClass.java
@@ -8,15 +8,10 @@ public class test_PatientClass {
 
     @Test
     public void test_createPatient() {
-        Patient derp = new Patient("Derp", "DER", "reole", 33, "rr", "S");
-
+        Patient derp = new Patient("Derp", "DER", "reole@derp.com"
+                ,33, "rr", "S");
         assertEquals(derp.getName(), "Derp");
-
-
-
-
 //        assertEquals(4, 2 + 2);
-
     }
 
 }
