4836b0055e01b05bb054ec5d4008c66e4b291a89
Jiayue Wu <EricWu_CHN@live.com>
2018-11-14 21:18:01 -0700

    homepage tab samples

---
+++ Part 4/app/src/main/java/com/example/jiayuewu/healthcarer_homepage/homepage_my_body_content.java
@@ -0,0 +1,29 @@
+package com.example.jiayuewu.healthcarer_homepage;
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+public class homepage_my_body_content extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.homepage_patient_my_body, container, false);
+        return rootView;
+    }
+}
---
+++ Part 4/app/src/main/java/com/example/jiayuewu/healthcarer_homepage/homepage_my_profile_content.java
@@ -0,0 +1,17 @@
+package com.example.jiayuewu.healthcarer_homepage;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class homepage_my_profile_content extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.homepage_patient_my_profile, container, false);
+        return rootView;
+    }
+}
