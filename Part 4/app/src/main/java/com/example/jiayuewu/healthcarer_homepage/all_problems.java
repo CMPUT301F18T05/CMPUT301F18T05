4f59a50c5585dd19629df4fc1fa12c2314c5572e
Hschhaniyara <hschhaniyara@gmail.com>
2018-11-20 13:41:37 -0700

    some fixz

--- Part 4/app/src/main/java/com/example/jiayuewu/healthcarer_homepage/all_problems.java
+++ Part 4/app/src/main/java/com/example/jiayuewu/healthcarer_homepage/all_problems.java
@@ -15,6 +15,7 @@ public class all_problems extends AppCompatActivity {
     private Problem problem = new Problem();
     private ArrayList<Problem> problemArrayList = new ArrayList<>();
     private ArrayAdapter<Problem> adapter;
+    private User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,23 +28,28 @@ public class all_problems extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Integer ID = problem.getProblemID();
+        user = DataHolder.getData();
+        Integer ID = user.getUserID();
         elasticSearch.getProblemsTask getProblemsTask = new elasticSearch.getProblemsTask();
         getProblemsTask.execute(ID);
+
         try{
             problemArrayList = getProblemsTask.get();
         } catch (Exception e){
             Log.e("Error", "Failed to get the problem history out of the async object.");
         }
+
         adapter = new ArrayAdapter<>(this, R.layout.activity_all_problems, problemArrayList);
         ListView history = findViewById(R.id.search_result_lisview);
         history.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
         history.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> arg0, View view, int position, long id) {
                 Intent viewAndEdit = new Intent(all_problems.this, view_patient_problem.class);
                 viewAndEdit.putExtra("position",position);
                 startActivity(viewAndEdit);
+
             }
         });
     }
