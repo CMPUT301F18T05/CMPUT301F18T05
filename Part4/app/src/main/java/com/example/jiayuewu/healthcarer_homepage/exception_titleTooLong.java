a25ca60bac444dd3af9bea982298797c6b6c2d69
gneagu <gneagu@users.noreply.github.com>
2018-11-29 23:58:55 -0700

    Final Commit of the NIght

--- Part4/app/src/main/java/com/example/jiayuewu/healthcarer_homepage/exception_titleTooLong.java
+++ Part4/app/src/main/java/com/example/jiayuewu/healthcarer_homepage/exception_titleTooLong.java
@@ -1,4 +1,14 @@
 package com.example.jiayuewu.healthcarer_homepage;
 
-public class exception_titleTooLong {
+public class exception_titleTooLong extends Exception{
+    public exception_titleTooLong() {
+    }
+
+    public exception_titleTooLong(String message) {
+        super(message);
+    }
+
+    public exception_titleTooLong(String message, Throwable throwable) {
+        super(message, throwable);
+    }
 }
